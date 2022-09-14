4ce30cc18d8123df95f31d733d18ca3f22d06997
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-09-14 18:43:17 -0300

    Iniciando projeto - Requisito 1 ao 9

---
+++ script.js
@@ -0,0 +1,12 @@
+function onClickLogin() {
+  let email = "tryber@teste.com";
+  let password = "123456";
+  const inputEmail = document.getElementById("email");
+  const inputPassword = document.getElementById("password");
+
+  if (email === inputEmail.value && password === inputPassword.value) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+}
