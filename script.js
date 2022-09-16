b6bb4f2cb50ea85f695b7eaca28a4b1ea71bbb3b
Isadora Araújo <isadora.sga@gmail.com>
2022-09-15 22:37:23 -0300

    corrige problemas lint

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 function onClickLogin() {
-  const email = "tryber@teste.com";
-  const password = "123456";
-  const inputEmail = document.getElementById("email");
-  const inputPassword = document.getElementById("password");
+  const email = 'tryber@teste.com';
+  const password = '123456';
+  const inputEmail = document.getElementById('email');
+  const inputPassword = document.getElementById('password');
 
   if (email === inputEmail.value && password === inputPassword.value) {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
 onClickLogin();
 
-function desabilitar(el) {
-  document.getElementById("agreement").disabled = !el.checked;
-  document.getElementById("submit-btn").disabled = "";
+function desabilitar() {
+  const agreement = document.getElementById('agreement');
+  const button = document.getElementById('submit-btn');
+  button.disabled = !agreement.checked;
+  button.disabled = '';
 
-  if (document.getElementById("agreement").checked == false) {
-    document.getElementById("submit-btn").disabled = "disabled";
+  if (document.getElementById('agreement').checked === false) {
+    document.getElementById('submit-btn').disabled = 'disabled';
   }
 }
+desabilitar();
