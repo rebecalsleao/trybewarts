57552182b8c04713187c5e621e888067c284464d
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-09-20 15:56:56 -0300

    Projeto Requisito 21 sento feito

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function onClickLogin() {
     alert('Email ou senha inválidos.');
   }
 }
-onClickLogin();
 
 function desabilitar() {
   const agreement = document.getElementById('agreement');
@@ -34,4 +33,19 @@ const countCharacters = () => {
 };
 textArea.addEventListener('input', countCharacters);
 
+function formData() {
+  const nameValue = document.getElementById('input-name').value;
+  const lastNameValue = document.getElementById('input-lastname').value;
+  const emailValue = document.getElementById('input-email').value;
+  const houseValue = document.getElementById('house').value;
+  const familyValue = document.getElementById('evaluation-form').elements['family'].value;
 
+  const formValue = {
+    name: nameValue,
+    lastName: lastNameValue,
+    email: emailValue,
+    house: houseValue,
+    family: familyValue,
+  };
+  console.log(formValue);
+}
